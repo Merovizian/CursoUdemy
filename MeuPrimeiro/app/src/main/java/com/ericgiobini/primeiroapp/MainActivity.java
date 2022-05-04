6669e4446aabcd29e78e4126659e7d3b46e7bb7a
Merovizian <ericgmicaela@gmail.com>
2022-05-04 15:50:51 -0300

    Versao 1

--- MeuPrimeiro/app/src/main/java/com/ericgiobini/primeiroapp/MainActivity.java
+++ MeuPrimeiro/app/src/main/java/com/ericgiobini/primeiroapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.ericgiobini.primeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.textoExibicao);
+        texto.setText("Eric Giobini Micaela");
+
+    }
+
 }
 
 
