ade97c3340b95a01bdd977bbd23e5e90af1d5be7
Merovizian <ericgmicaela@gmail.com>
2022-05-09 15:45:41 -0300

    Version 2

--- projetosorteios6/app/src/main/java/com/ericgiobini/projetosorteios6/MainActivity.java
+++ projetosorteios6/app/src/main/java/com/ericgiobini/projetosorteios6/MainActivity.java
@@ -3,6 +3,10 @@ package com.ericgiobini.projetosorteios6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ativar_botao (View view){
+
+        TextView texto = findViewById(R.id.texto_jogo);
+
+        int numero = new Random().nextInt(11);
+
+
+        texto.setText( "Voce Sorteou: " + numero);
+
+    }
+
 }
 
-
-// TESTANDO O GIT
