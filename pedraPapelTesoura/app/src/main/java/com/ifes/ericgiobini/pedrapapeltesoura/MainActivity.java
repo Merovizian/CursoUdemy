b9b196c2b1df326a60203c6fa6e8bc94f561cd3d
Merovizian <ericgmicaela@gmail.com>
2022-06-03 14:56:23 -0300

    Quem é o melhor filho?  V.final

--- pedraPapelTesoura/app/src/main/java/com/ifes/ericgiobini/pedrapapeltesoura/MainActivity.java
+++ pedraPapelTesoura/app/src/main/java/com/ifes/ericgiobini/pedrapapeltesoura/MainActivity.java
@@ -14,45 +14,60 @@ public class MainActivity extends AppCompatActivity {
 
     private String escolhaPaiMae;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
-
-    public void selecionadoPai(View view){
+    public void selecionadoPai(View view) {
         escolhaPaiMae = "pai";
+        ImageView imagemResultadoPaiMae = findViewById(R.id.resultadoEscolhaPaiMae);
+        switch (escolhaPaiMae) {
+            case "mae":
+                imagemResultadoPaiMae.setImageResource(R.drawable.mae);
+                break;
+            case "pai":
+                imagemResultadoPaiMae.setImageResource(R.drawable.pai);
+                break;
+        }
 
     }
+
     public void selecionadoMae(View view) {
         escolhaPaiMae = "mae";
+        ImageView imagemResultadoPaiMae = findViewById(R.id.resultadoEscolhaPaiMae);
+        switch (escolhaPaiMae) {
+            case "mae":
+                imagemResultadoPaiMae.setImageResource(R.drawable.mae);
+                break;
+            case "pai":
+                imagemResultadoPaiMae.setImageResource(R.drawable.pai);
+                break;
+        }
 
     }
 
-    public void selecionadoBruno(View view){
+    public void selecionadoBruno(View view) {
         this.opcaoSelecionada("Primogênito");
 
     }
-    public void selecionadoEric(View view){
+
+    public void selecionadoEric(View view) {
         this.opcaoSelecionada("FilhodoMeio");
 
     }
-    public void selecionadoCarolina(View view){
+
+    public void selecionadoCarolina(View view) {
         this.opcaoSelecionada("Preferida");
 
     }
 
 
-
-
-    public void opcaoSelecionada(String stringSelecionada){
-
-
-
-
+    public void opcaoSelecionada(String stringSelecionada) {
 
 
         ImageView imagemResultadoComputador = findViewById(R.id.resultadoComputador);
@@ -66,11 +81,10 @@ public class MainActivity extends AppCompatActivity {
         nomeEscolhaCelular.setText("Celular Escolheu:");
 
 
-
-
-
         int escolhaComputador = new Random().nextInt(3);
-        String [] opcoes = {"Primogênito", "FilhodoMeio", "Preferida"};
+
+
+        String[] opcoes = {"Primogênito", "FilhodoMeio", "Preferida"};
         String opcaoComputador = opcoes[escolhaComputador];
 
         switch (opcaoComputador) {
@@ -102,25 +116,44 @@ public class MainActivity extends AppCompatActivity {
         escolhaCelular.setText(opcaoComputador);
 
 
-
-        if (opcaoComputador == "Primogênito" && stringSelecionada == "FilhodoMeio"){
-            textoResultado.setText("CELULAR GANHOU!! O filho do meio conseguiria no máximo um empate, se nascesse mulher");
-        }else if(opcaoComputador == "Primogênito" && stringSelecionada == "Preferida"){
-            textoResultado.setText("EMPATE!! A filha preferida tem vantagem sobre todos, menos com o primogênito");
-        }else if(opcaoComputador == "Preferida" && stringSelecionada == "FilhodoMeio"){
-            textoResultado.setText("CELULAR GANHOU!! O filho do meio já nasceu perdendo");
-        }else if (opcaoComputador == "Preferida" && stringSelecionada == "Primogênito"){
-            textoResultado.setText("EMPATE!! o primogênito só ganharia se a filha predileta fosse a do meio.");
-        }else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Preferida"){
-            textoResultado.setText("JOGADOR GANHOU!! A filha tem seu proprio quarto, tem como competir com isso?");
-        }else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Primogênito"){
-            textoResultado.setText("JOGADOR GANHOU!! O primogênito dominou sozinho por 4 anos. Coitado do filho do meio");
-        } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "FilhodoMeio"){
-            textoResultado.setText("OS DOIS PERDERAM!! Não há vencedores quando se é o Filho do meio, só derrota.");
-        } else {
-            textoResultado.setText("VITORIA DOS DOIS!!");
+        if (escolhaPaiMae == "mae") {
+            if (opcaoComputador == "Primogênito" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("CELULAR GANHOU!! O filho do meio só ganha se nascesse mulher");
+            } else if (opcaoComputador == "Primogênito" && stringSelecionada == "Preferida") {
+                textoResultado.setText("JOGADOR GANHOU O primeiro filho só ganharia se nascesse mulher");
+            } else if (opcaoComputador == "Preferida" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("CELULAR GANHOU!! ASUHEUAHSEUS O filho do meio só existe pq nasceu Homem");
+            } else if (opcaoComputador == "Preferida" && stringSelecionada == "Primogênito") {
+                textoResultado.setText("CELULAR GANHOU!! O primeiro filho foi preferido por 9 anos");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Preferida") {
+                textoResultado.setText("JOGADOR GANHOU!! A filha tem seu proprio quarto, tem como competir com isso?");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Primogênito") {
+                textoResultado.setText("JOGADOR GANHOU!! O Primogênito escravizou Eric por anos e anos ");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("OS DOIS PERDERAM!! Não há vencedores quando se é o Filho do meio, só derrota.");
+            } else {
+                textoResultado.setText("VITORIA DOS DOIS!!");
+            }
         }
-
+        if (escolhaPaiMae == "pai") {
+            if (opcaoComputador == "Primogênito" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("CELULAR GANHOU!! O filho do meio conseguiria no máximo um empate, se nascesse mulher");
+            } else if (opcaoComputador == "Primogênito" && stringSelecionada == "Preferida") {
+                textoResultado.setText("EMPATE!! A filha preferida tem vantagem sobre todos, menos com o primogênito");
+            } else if (opcaoComputador == "Preferida" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("CELULAR GANHOU!! O filho do meio já nasceu perdendo");
+            } else if (opcaoComputador == "Preferida" && stringSelecionada == "Primogênito") {
+                textoResultado.setText("EMPATE!! o primogênito só ganharia se a filha predileta fosse a do meio.");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Preferida") {
+                textoResultado.setText("JOGADOR GANHOU!! A filha tem seu proprio quarto, tem como competir com isso?");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "Primogênito") {
+                textoResultado.setText("JOGADOR GANHOU!! O primogênito dominou sozinho por 4 anos. Coitado do filho do meio");
+            } else if (opcaoComputador == "FilhodoMeio" && stringSelecionada == "FilhodoMeio") {
+                textoResultado.setText("OS DOIS PERDERAM!! Não há vencedores quando se é o Filho do meio, só derrota.");
+            } else {
+                textoResultado.setText("VITORIA DOS DOIS!!");
+            }
+        }
     }
 }
 
