a1667f877cb80e29daadb57f9cc5e258a4c5640f
Merovizian <ericgmicaela@gmail.com>
2022-06-21 16:58:01 -0300

    Listagem de itens.

---
+++ ListView/app/src/main/java/com/ifes/ericgiobini/listview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.ifes.ericgiobini.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {"Angra dos Reis","Caldas Novas","Campos do Jordão", "Costa do Sauípe", "Ilhéus","Aruba"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.list_Locais);
+
+        // Cria adaptador para a lista.
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        // Adiciona adaptador para a lista
+        listLocais.setAdapter( adaptador );
+
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String valorSelecionado = (String) listLocais.getItemAtPosition(i);
+                Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
