bc94f6b9570aecc4b796bc02490a3c20ae683df1
Merovizian <ericgmicaela@gmail.com>
2022-09-14 10:24:12 -0300

    Aplicativo que salva um nome no dispositivo. (SharedPreferences, TextInputEditText

---
+++ PreferenciasUsuario/app/src/main/java/ifes/eric/preferenciasusuario/MainActivity.java
@@ -0,0 +1,62 @@
+package ifes.eric.preferenciasusuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+    private TextInputEditText nomeUsuario;
+    private Button botaoSalvar;
+    private TextView nomeSalvo;
+    private final String ARQUIVO_PREFERENCIA = "BancoDados";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoSalvar = findViewById(R.id.button_salvar);
+        nomeUsuario = findViewById(R.id.input_nome);
+        nomeSalvo = findViewById(R.id.text_usuariosalvo);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+
+                // Ferramenta para salvar preferencias no dispositivo
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                if (nomeUsuario.getText().toString().equals("")){
+                    Toast.makeText(getApplication(), "Digite o Nome!", Toast.LENGTH_LONG).show();}
+                else{
+                    String nome = nomeUsuario.getText().toString();
+                    editor.putString("nome", nome);
+                    editor.apply();
+                    Toast.makeText(getApplication(), "Usuario " + nomeUsuario.getText() + " Salvo!", Toast.LENGTH_LONG).show();
+                    nomeSalvo.setText("Olá" + nome);
+
+                }
+                }
+        });
+
+
+    }
+
+
+
+
+
+
+
+}
