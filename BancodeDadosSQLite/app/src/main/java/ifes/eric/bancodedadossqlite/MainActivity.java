814fca3a13d08b411564c6dde23499d1f6f1da42
Merovizian <ericgmicaela@gmail.com>
2022-09-21 13:45:51 -0300

    Criação de BANCO DE DADOS: TABELA, INSERÇÃO, RECUPERAÇÃO, TIPOS DE BUSCA

--- BancodeDadosSQLite/app/src/main/java/ifes/eric/bancodedadossqlite/MainActivity.java
+++ BancodeDadosSQLite/app/src/main/java/ifes/eric/bancodedadossqlite/MainActivity.java
@@ -10,13 +10,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textozinho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textozinho = findViewById(R.id.text_inout);
 
         try {
             // Cria o banco de dados
@@ -25,66 +23,68 @@ public class MainActivity extends AppCompatActivity {
 
             // Cria uma tabela
             bancoDados.execSQL("DROP TABLE IF EXISTS pessoas");
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS" +
+                    " pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3))");
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ POPULAÇÃO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             // Inserir dados | CRIADOR DE BANCO DE DADOS EXCEL
             bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES" +
                     "   (   'Eric Giobini Micaela'  ,   33  )   ,   "   +
-                    "	(	'Bianca Nogueira	'	,	58	)	,	"	+
-                    "	(	'Daniela Barbosa	'	,	38	)	,	"	+
-                    "	(	'Lavínia Aragão	'	,	16	)	,	"	+
-                    "	(	'Yago da Cruz	'	,	62	)	,	"	+
-                    "	(	'Vitor Gabriel Barros	'	,	22	)	,	"	+
-                    "	(	'Bernardo da Conceição	'	,	50	)	,	"	+
-                    "	(	'João Felipe Barros	'	,	53	)	,	"	+
-                    "	(	'Sr. Kaique da Cunha	'	,	60	)	,	"	+
-                    "	(	'Ana Clara da Luz	'	,	10	)	,	"	+
-                    "	(	'Isadora da Paz	'	,	19	)	,	"	+
-                    "	(	'João Guilherme Ferreira	'	,	6	)	,	"	+
-                    "	(	'Luana Duarte	'	,	33	)	,	"	+
-                    "	(	'Isaac Carvalho	'	,	25	)	,	"	+
-                    "	(	'Pedro Miguel Alves	'	,	24	)	,	"	+
-                    "	(	'Vicente Silveira	'	,	34	)	,	"	+
-                    "	(	'João Felipe Rocha	'	,	45	)	,	"	+
-                    "	(	'Ana Beatriz Fogaça	'	,	35	)	,	"	+
-                    "	(	'Vitor Gabriel Silveira	'	,	16	)	,	"	+
-                    "	(	'Dr. Antônio Ribeiro	'	,	36	)	,	"	+
-                    "	(	'Mariane da Mata	'	,	10	)	,	"	+
-                    "	(	'Paulo Cardoso	'	,	62	)	,	"	+
-                    "	(	'Emanuella Rodrigues	'	,	42	)	,	"	+
-                    "	(	'Sra. Evelyn Moraes	'	,	63	)	,	"	+
-                    "	(	'Enzo Moraes	'	,	60	)	,	"	+
-                    "	(	'João Pedro Duarte	'	,	12	)	,	"	+
-                    "	(	'Maysa Gonçalves	'	,	8	)	,	"	+
-                    "	(	'Kaique Barbosa	'	,	83	)	,	"	+
-                    "	(	'João Guilherme Nunes	'	,	63	)	,	"	+
-                    "	(	'Juan Araújo	'	,	45	)	,	"	+
-                    "	(	'Lorenzo da Luz	'	,	48	)	,	"	+
-                    "	(	'Dra. Natália Oliveira	'	,	37	)	,	"	+
-                    "	(	'Alícia Cavalcanti	'	,	82	)	,	"	+
-                    "	(	'Olivia da Cunha	'	,	50	)	,	"	+
-                    "	(	'Stella Pereira	'	,	10	)	,	"	+
-                    "	(	'Maitê Caldeira	'	,	25	)	,	"	+
-                    "	(	'Ana Sophia Caldeira	'	,	29	)	,	"	+
-                    "	(	'Luiza Santos	'	,	65	)	,	"	+
-                    "	(	'Raul da Rosa	'	,	50	)	,	"	+
-                    "	(	'Dra. Larissa Vieira	'	,	52	)	,	"	+
-                    "	(	'Henrique Jesus	'	,	8	)	,	"	+
-                    "	(	'Erick Cardoso	'	,	63	)	,	"	+
-                    "	(	'Sr. Nathan da Mata	'	,	80	)	,	"	+
-                    "	(	'Sra. Maria Sophia da Cruz	'	,	59	)	,	"	+
-                    "	(	'Paulo Mendes	'	,	78	)	,	"	+
-                    "	(	'Olivia Costa	'	,	10	)	,	"	+
-                    "	(	'Gustavo Henrique Gomes	'	,	52	)	,	"	+
-                    "	(	'Gustavo Henrique Lima	'	,	48	)	,	"	+
-                    "	(	'Levi Azevedo	'	,	42	)	,	"	+
-                    "	(	'Sr. Davi Luiz Ribeiro	'	,	72	)	,	"	+
-                    "	(	'Igor Barros	'	,	71	)");
+                    "	(	'Bianca Nogueira'	,	58	)	,	"	+
+                    "	(	'Daniela Barbosa'	,	38	)	,	"	+
+                    "	(	'Lavínia Aragão'	,	16	)	,	"	+
+                    "	(	'Yago da Cruz'	,	62	)	,	"	+
+                    "	(	'Vitor Gabriel Barros'	,	22	)	,	"	+
+                    "	(	'Bernardo da Conceição'	,	50	)	,	"	+
+                    "	(	'João Felipe Barros'	,	53	)	,	"	+
+                    "	(	'Sr. Kaique da Cunha'	,	60	)	,	"	+
+                    "	(	'Ana Clara da Luz'	,	10	)	,	"	+
+                    "	(	'Isadora da Paz'	,	19	)	,	"	+
+                    "	(	'João Guilherme Ferreira'	,	6	)	,	"	+
+                    "	(	'Luana Duarte'	,	33	)	,	"	+
+                    "	(	'Isaac Carvalho'	,	25	)	,	"	+
+                    "	(	'Pedro Miguel Alves'	,	24	)	,	"	+
+                    "	(	'Vicente Silveira'	,	34	)	,	"	+
+                    "	(	'João Felipe Rocha'	,	45	)	,	"	+
+                    "	(	'Ana Beatriz Fogaça'	,	35	)	,	"	+
+                    "	(	'Vitor Gabriel Silveira'	,	16	)	,	"	+
+                    "	(	'Dr. Antônio Ribeiro'	,	36	)	,	"	+
+                    "	(	'Mariane da Mata'	,	10	)	,	"	+
+                    "	(	'Paulo Cardoso'	,	62	)	,	"	+
+                    "	(	'Emanuella Rodrigues'	,	42	)	,	"	+
+                    "	(	'Sra. Evelyn Moraes'	,	63	)	,	"	+
+                    "	(	'Enzo Moraes'	,	60	)	,	"	+
+                    "	(	'João Pedro Duarte'	,	12	)	,	"	+
+                    "	(	'Maysa Gonçalves'	,	8	)	,	"	+
+                    "	(	'Kaique Barbosa'	,	83	)	,	"	+
+                    "	(	'João Guilherme Nunes'	,	63	)	,	"	+
+                    "	(	'Juan Araújo'	,	45	)	,	"	+
+                    "	(	'Lorenzo da Luz'	,	48	)	,	"	+
+                    "	(	'Dra. Natália Oliveira'	,	37	)	,	"	+
+                    "	(	'Alícia Cavalcanti'	,	82	)	,	"	+
+                    "	(	'Olivia da Cunha'	,	50	)	,	"	+
+                    "	(	'Stella Pereira'	,	10	)	,	"	+
+                    "	(	'Maitê Caldeira'	,	25	)	,	"	+
+                    "	(	'Ana Sophia Caldeira'	,	29	)	,	"	+
+                    "	(	'Luiza Santos'	,	65	)	,	"	+
+                    "	(	'Raul da Rosa'	,	50	)	,	"	+
+                    "	(	'Dra. Larissa Vieira'	,	52	)	,	"	+
+                    "	(	'Henrique Jesus'	,	8	)	,	"	+
+                    "	(	'Erick Cardoso'	,	63	)	,	"	+
+                    "	(	'Sr. Nathan da Mata'	,	80	)	,	"	+
+                    "	(	'Sra. Maria Sophia da Cruz'	,	59	)	,	"	+
+                    "	(	'Paulo Mendes'	,	78	)	,	"	+
+                    "	(	'Olivia Costa'	,	10	)	,	"	+
+                    "	(	'Gustavo Henrique Gomes'	,	52	)	,	"	+
+                    "	(	'Gustavo Henrique Lima'	,	48	)	,	"	+
+                    "	(	'Levi Azevedo'	,	42	)	,	"	+
+                    "	(	'Sr. Davi Luiz Ribeiro'	,	72	)	,	"	+
+                    "	(	'Igor Barros'	,	71	)	"	)	;
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ EXEMPLOS DE CONSULTAS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
             // Recuperar Dados | EXEMPLOS DE Consultas
             /*
@@ -92,10 +92,12 @@ public class MainActivity extends AppCompatActivity {
                     " FROM pessoas" +
                     " WHERE idade < 18 OR idade > 65";*/
 
+
             /*
             String consulta = "SELECT nome, idade" +
                     " FROM pessoas" +
                     " WHERE idade in(19,24,07,25)";*/
+
 
             /*
             String consulta = "SELECT nome, idade" +
@@ -111,35 +113,52 @@ public class MainActivity extends AppCompatActivity {
                     " FROM pessoas" +
                     " WHERE idade >= 18 AND nome LIKE '%"+filtro+"%'";*/
 
+
             // ORDENAÇÃO - SEM NADA, ASC, DESC
             /*String consulta = "SELECT nome, idade" +
                     " FROM pessoas" +
                     " ORDER BY idade DESC";*/
 
+
             /*String consulta = "SELECT nome, idade" +
                     " FROM pessoas" +
                     " ORDER BY nome";*/
+
+
             // LIMITE
             /*String consulta = "SELECT nome, idade" +
                     " FROM pessoas" +
                     " LIMIT 3";*/
 
+
             // TODOS OS MANIPULADORES
-            String consulta = "SELECT nome, idade" +
+            /*String consulta = "SELECT nome, idade" +
                     " FROM pessoas" +
                     " WHERE idade BETWEEN 18 and 65" +
                     " ORDER BY idade" +
-                    " LIMIT 5";
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    " LIMIT 5";*/
 
 
-            Cursor cursor = bancoDados.rawQuery(consulta, null);
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ATUALIZANDO E REMOVENDO REGISTROS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+        //bancoDados.execSQL("UPDATE pessoas SET idade = 19 WHERE nome LIKE 'Eric%'");
+        //bancoDados.execSQL("UPDATE pessoas SET idade = 18, nome = 'JESUS' WHERE nome LIKE 'Eric%'")
+        //bancoDados.execSQL("DELETE FROM pessoas WHERE id = 1");
+        //bancoDados.execSQL("DELETE FROM pessoas");
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PRINTANDO OS RESULTADOS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //  '*' define todos os campos
+            Cursor cursor = bancoDados.rawQuery("SELECT * " +
+                    " FROM pessoas",null);
 
 
             // Indices da tabela
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
+            int indiceCod = cursor.getColumnIndex("id");
+
 
             // Iniciar o indice no inicio
             cursor.moveToFirst();
@@ -147,10 +166,10 @@ public class MainActivity extends AppCompatActivity {
             while (cursor != null){
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
+                String id = cursor.getString(indiceCod);
 
-                textozinho.setText(nome + "," + idade);
-
-                Log.i("Resultado", ">> nome: " + nome +", idade: "+ idade);
+                Log.i("Resultado", ">> nome: " + nome +" | idade: "+ idade +
+                        " | id: " + id);
                 cursor.moveToNext();
             }
 
