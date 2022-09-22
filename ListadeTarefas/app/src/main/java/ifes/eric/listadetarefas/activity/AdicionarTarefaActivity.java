d84d1bc0e64d75841e4cf0f7365b8a19969ba2ad
Merovizian <ericgmicaela@gmail.com>
2022-09-22 14:37:14 -0300

    Adapter, RecyclerView, toolbar, FAB, putExtra. Evento de clique Recycler. Menu Superior Direito, itens, icons

--- ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/AdicionarTarefaActivity.java
+++ ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package ifes.eric.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import ifes.eric.listadetarefas.R;
 
@@ -12,5 +16,39 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_salvar:
+
+                break;
+            case R.id.action_editar:
+
+                break;
+            case R.id.action_configuracao:
+
+                break;
+
+
+
+
+
+
+
+
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
