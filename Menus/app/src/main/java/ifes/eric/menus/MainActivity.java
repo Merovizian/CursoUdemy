505e12e93f244611283805150ec10247604d4798
Merovizian <ericgmicaela@gmail.com>
2022-09-22 14:15:37 -0300

    Criação de um menu e personalização dos Itens (superior direito)

--- Menus/app/src/main/java/ifes/eric/menus/MainActivity.java
+++ Menus/app/src/main/java/ifes/eric/menus/MainActivity.java
@@ -1,9 +1,13 @@
 package ifes.eric.menus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         //Inflar transforma um objeto XML em objeto do tipo view
         getMenuInflater().inflate(R.menu.menu_principal, menu);
-
-
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_configurações:
+                Toast.makeText(this, "Item Configurações", Toast.LENGTH_SHORT).show();
+
+
+                break;
+            case R.id.item_editar:
+                Toast.makeText(this, "Item Editar", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.item_salvar:
+                Toast.makeText(this, "Item Salvar", Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
