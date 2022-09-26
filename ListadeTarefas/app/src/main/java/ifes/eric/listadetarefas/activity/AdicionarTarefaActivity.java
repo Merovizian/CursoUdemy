dce542487fc5f3b625a0ad59594adc2ca60fc860
Merovizian <ericgmicaela@gmail.com>
2022-09-26 11:05:25 -0300

    Adapter, RecyclerView, toolbar, FAB, putExtra. Evento de clique Recycler. Menu Superior Direito, itens, icons BANCO DE DADOS

--- ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/AdicionarTarefaActivity.java
+++ ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,71 +1,16 @@
 package ifes.eric.listadetarefas.activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import ifes.eric.listadetarefas.R;
-import ifes.eric.listadetarefas.helper.TarefaDAO;
-import ifes.eric.listadetarefas.model.Tarefa;
 
 public class AdicionarTarefaActivity extends AppCompatActivity {
-
-    private TextInputEditText editTarefa;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
-
-        editTarefa = findViewById(R.id.input_tarefa);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.action_salvar:
-                TarefaDAO tarefaDAO = new TarefaDAO(getApplicationContext());
-
-                Tarefa tarefa = new Tarefa();
-                tarefa.setNomeTarefa("Ir ao mercado");
-                tarefaDAO.salvar(tarefa);
-
-
-
-                break;
-            case R.id.action_editar:
-
-                break;
-            case R.id.action_configuracao:
-
-                break;
-
-
-
-
-
-
-
-
-
-        }
-
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/MainActivity.java
+++ ListadeTarefas/app/src/main/java/ifes/eric/listadetarefas/activity/MainActivity.java
@@ -1,42 +1,29 @@
 package ifes.eric.listadetarefas.activity;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.util.Log;
 import android.view.View;
 
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import ifes.eric.listadetarefas.R;
-import ifes.eric.listadetarefas.adapter.TarefaAdapter;
 import ifes.eric.listadetarefas.databinding.ActivityMainBinding;
-import ifes.eric.listadetarefas.helper.DbHelper;
-import ifes.eric.listadetarefas.helper.RecyclerItemClickListener;
-import ifes.eric.listadetarefas.model.Tarefa;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private TarefaAdapter tarefaAdapter;
-    private List<Tarefa> listaTarefas = new ArrayList<>();
-
+    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,104 +34,60 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        // CONFIGURAR O RECYCLER VIEW
+        // Busca o Recycler pelo id.
         recyclerView = findViewById(R.id.recycler_listaTarefas);
 
-
-        // Adicionar evento de clique
-        recyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(
-                        getApplicationContext(),
-                        recyclerView,
-                        new RecyclerItemClickListener.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                Log.i("Clique","onItemClick" + "| item: " + position);
-                            }
-
-                            @Override
-                            public void onLongItemClick(View view, int position) {
-                                Log.i("Clique", "onLongItemClick");
-                            }
-
-                            @Override
-                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                            }
-                        }
-                )
-
-
-        );
 
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                // Objeto que facilita a comunicação com outros componentes.
-                Intent intent = new Intent(getApplicationContext(), AdicionarTarefaActivity.class);
-                startActivity(intent);
+                // Cria uma activity e a vincula ao FAB button
+                startActivity(new Intent(getApplicationContext(), AdicionarTarefaActivity.class));
 
             }
         });
     }
 
+    // METODO PARA O RECYCLERVIEW - monta as tarefas.
+    public void carregarListaTarefas (){
 
-    public void carregarListaTarefas(){
+        // LISTAR TAREFAS
 
-        // Listar Tarefas
-        Tarefa tarefa1 = new Tarefa();
-        tarefa1.setNomeTarefa("Ir ao mercado");
-        listaTarefas.add (tarefa1);
-        Tarefa tarefa12 = new Tarefa();
-        tarefa12.setNomeTarefa("Ir ao cinema");
-        listaTarefas.add (tarefa12);
 
-        // Configurar Adapter
-        tarefaAdapter = new TarefaAdapter(listaTarefas);
+        // CONFIGURAR ADAPTER
 
-        // Configurar Recycler
+
+        // CONFIGURAR RECYCLER
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
-        recyclerView.setAdapter(tarefaAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(),
+                LinearLayout.VERTICAL));
+        //recyclerView.setAdapter(adapter);
+
+
+
 
 
     }
 
-
-
     @Override
-    protected void onStart() {
-        carregarListaTarefas();
-        Toast.makeText(this,"onStart", Toast.LENGTH_SHORT).show();
-        super.onStart();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
-    protected void onRestart() {
-        Toast.makeText(this,"onRestart", Toast.LENGTH_SHORT).show();
-        super.onRestart();
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    @Override
-    protected void onPause() {
-        Toast.makeText(this,"onPause", Toast.LENGTH_SHORT).show();
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Toast.makeText(this,"onStop", Toast.LENGTH_SHORT).show();
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Toast.makeText(this,"onDestroy", Toast.LENGTH_SHORT).show();
-        super.onDestroy();
+        return super.onOptionsItemSelected(item);
     }
 
 }
