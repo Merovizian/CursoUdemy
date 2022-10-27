15da5761646b2dedebf4e94d2a1978b4d0b7cd0f
Merovizian <ericgmicaela@gmail.com>
2022-10-27 11:07:12 -0300

    App com Firebase, slider

--- Organizze/app/src/main/java/ifes/eric/organizze/MainActivity.java
+++ Organizze/app/src/main/java/ifes/eric/organizze/MainActivity.java
@@ -2,7 +2,9 @@ package ifes.eric.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,8 +12,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
-public class MainActivity extends AppCompatActivity {
+import ifes.eric.organizze.activity.cadastroActivity;
+import ifes.eric.organizze.activity.loginActivity;
+
+public class MainActivity extends IntroActivity {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  FIREBASEAUTH   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Declara uma instancia FirebaseAuth.
     private FirebaseAuth mAuth;
@@ -20,36 +27,65 @@ public class MainActivity extends AppCompatActivity {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   DATABASE      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Mensagem");
-
 //*****************************************   DATABASE     *****************************************
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Slides      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+//******************************************   Slides     ******************************************
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Slides Fragments     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .fragment(R.layout.intro_um)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .fragment(R.layout.intro_dois)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .fragment(R.layout.intro_tres)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .fragment(R.layout.intro_quatro)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.teal_200)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build()
+        );
+
+//*************************************   Slides Fragments     *************************************
+
+
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  FIREBASEAUTH   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         // Declara uma instancia FirebaseAuth.
         mAuth = FirebaseAuth.getInstance();
-
-
-
 //*****************************************  FIREBASEAUTH  *****************************************
-
-        TextView revelação = findViewById(R.id.text_texto);
-        revelação.setText(mAuth.toString());
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null){
-            Toast.makeText(this, currentUser.toString(), Toast.LENGTH_SHORT).show();
-        }else
-        {
-            Toast.makeText(this, "Usuario Não Logado", Toast.LENGTH_SHORT).show();
-        }
+    public void btEntrar(View view){
+        startActivity(new Intent(this,loginActivity.class));
     }
+    public void cadastrar(View view){
+        startActivity(new Intent(this, cadastroActivity.class));
+
+
+    }
+
 }
---
+++ Organizze/app/src/main/java/ifes/eric/organizze/activity/loginActivity.java
@@ -0,0 +1,15 @@
+package ifes.eric.organizze.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ifes.eric.organizze.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
