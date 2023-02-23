53e856150465996f360375028857bc6df37e14aa
Merovizian <ericgmicaela@gmail.com>
2023-02-23 15:10:54 -0300

    Firebase, troca de mainactivity,Toolbar, Menu. Abas, Fragments, Permissoes, Tirar e recuperar imagens.

--- Whatsapp/app/src/main/java/ifes/eric/whatsapp/Activity/ConfiguracoesActivity.java
+++ Whatsapp/app/src/main/java/ifes/eric/whatsapp/Activity/ConfiguracoesActivity.java
@@ -1,25 +1,38 @@
 package ifes.eric.whatsapp.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.Objects;
 import java.util.zip.Inflater;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import ifes.eric.whatsapp.R;
 import ifes.eric.whatsapp.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private CircleImageView imagePerfil;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_GALERRY = 2;
 
 
     //   ---------------------------  Lista de Permissoes solicitadas   --------------------------------
@@ -34,20 +47,85 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
 
+//   -----------------  Codigos que configuram os botoes de photo e galeria  -----------------------
+
+        imageButtonGaleria = findViewById(R.id.config_imageButton_gallery);
+        imageButtonCamera = findViewById(R.id. config_imageButton_photo);
+        imagePerfil = findViewById(R.id.configuracoes_circle_perfil);
+
+
+        // Evento de clique do botão, poderia ser iniciado como um metodo também
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pegarFoto();
+
+            }
+        });
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tirarFoto();
+
+            }
+        });
+
+//   *****************  Codigos que configuram os botoes de photo e galeria   **********************
+
 //   ---------------------------  Codigos que configuram a toolbar  --------------------------------
         Toolbar toolbar = findViewById(R.id.toolbar_principal);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
         // Cria o botão de retorno à activity "Pai"
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-//   *****************************  Codigos que configuram a toolbar  *******************************
+//   *****************************  Codigos que configuram a toolbar  ******************************
 
         // Valida as permissões
         Permissao.validarPermissoes(permissoesNecessarias, this, 1);
-
     }
 
-    //   ---------------------  Metodo que aponta os resultados das permissoes  ------------------------
+//   --------------------------  Metodo que recupera os dados salvos  -----------------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // caso o resultado da solicitação tenha dado certo.
+        if (resultCode == RESULT_OK){
+            //cria-se um objeto Bitmap
+            Bitmap imagem = null;
+
+            try {
+
+                switch (requestCode) {
+                    case REQUEST_IMAGE_CAPTURE:
+                        // O objeto Bitmap é direcionado ao que foi adquirido pelo data
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case REQUEST_IMAGE_GALERRY:
+                        // objeto que salva a URI da data
+                        Uri localImagemSelecionada = data.getData();
+                        // Objeto imagem busca esse data, na URI selecionada, content provem acesso
+                        // aos conteudos.
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(),localImagemSelecionada);
+                        break;
+                }
+                if (imagem != null){
+                    // Insere o bitmap Imagem no slot de perfil;
+                   imagePerfil.setImageBitmap(imagem);
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+//   **************************   Metodo que recupera os dados salvos   ***************************
+
+
+//   ---------------------  Metodo que aponta os resultados das permissoes  ------------------------
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -79,6 +157,28 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         builder.create();
         builder.show();
     }
+
+
+    // Metodo que acessa a camera
+    private void tirarFoto() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    // Metodo que acessa a galeria
+    private void pegarFoto() {
+
+        Intent takeGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (takeGalleryIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeGalleryIntent, REQUEST_IMAGE_GALERRY);
+        }
+
+    }
+
 
 
 }
