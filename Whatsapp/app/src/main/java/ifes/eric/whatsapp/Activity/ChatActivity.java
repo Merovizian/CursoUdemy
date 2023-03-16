842134e0c14b6b3eac9ae04493d5e8f67a9f1570
Merovizian <ericgmicaela@gmail.com>
2023-03-15 21:24:14 -0300

    Firebase, troca de mainActivity,Toolbar, Menu. Abas, Fragments, Permissoes, Tirar e recuperar imagens e textos, formas e desenhos de objetos. Putextra

--- Whatsapp/app/src/main/java/ifes/eric/whatsapp/Activity/ChatActivity.java
+++ Whatsapp/app/src/main/java/ifes/eric/whatsapp/Activity/ChatActivity.java
@@ -3,24 +3,13 @@ package ifes.eric.whatsapp.Activity;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-
-import java.util.Objects;
 
 import ifes.eric.whatsapp.R;
-import ifes.eric.whatsapp.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
     public FloatingActionButton fab;
@@ -29,9 +18,9 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_chat2);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
