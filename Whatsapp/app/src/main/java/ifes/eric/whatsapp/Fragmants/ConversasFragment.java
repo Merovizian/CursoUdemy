bbbd04cf27c1ce9a538ad0e85115397737ff4751
Merovizian <ericgmicaela@gmail.com>
2023-03-25 22:54:18 -0300

    Firebase, troca de mainActivity,Toolbar, Menu. Abas, Fragments, Permissoes, Tirar e recuperar imagens e textos, formas e desenhos de objetos. Putextra, RecyclerView

--- Whatsapp/app/src/main/java/ifes/eric/whatsapp/Fragmants/ConversasFragment.java
+++ Whatsapp/app/src/main/java/ifes/eric/whatsapp/Fragmants/ConversasFragment.java
@@ -2,65 +2,122 @@ package ifes.eric.whatsapp.Fragmants;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ifes.eric.whatsapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConversasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+import ifes.eric.whatsapp.Adapter.ConversasAdapter;
+import ifes.eric.whatsapp.Model.Conversa;
+import ifes.eric.whatsapp.R;
+import ifes.eric.whatsapp.helper.UserFacilities;
+
+
 public class ConversasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerViewConversas;
+    private List<Conversa> listaConversas = new ArrayList<>();
+    private ConversasAdapter conversasAdapter;
+    private DatabaseReference bancoDados = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference conversasREF;
+    private ChildEventListener childEventListenerConversas;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ConversasFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConversasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConversasFragment newInstance(String param1, String param2) {
-        ConversasFragment fragment = new ConversasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public ConversasFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversas, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversas, container, false);
+
+        recyclerViewConversas = view.findViewById(R.id.recycler_fragmentsConversas_recyclerContatos);
+
+        // configurar adapter
+        conversasAdapter = new ConversasAdapter(listaConversas, getActivity());
+
+
+
+        // configurar o recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewConversas.setLayoutManager(layoutManager);
+        recyclerViewConversas.setHasFixedSize(true);
+        recyclerViewConversas.setAdapter(conversasAdapter);
+
+        // Cria um link de referencia para a raiz de conversas no Firebase
+        String identificadorUsuario = UserFacilities.UsuarioEmailB64();
+        conversasREF = bancoDados.child("conversas")
+                .child(identificadorUsuario);
+
+
+        return view;
+    }
+
+//   ----------------------- Função que recupera os valores do Firebase e aplica no recycler ---------------------------
+    public void recuperarConversas (){
+
+        childEventListenerConversas = conversasREF.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // Acessa o banco de dados e recupera toda a classe de conversa.
+                Conversa conversa = snapshot.getValue(Conversa.class);
+
+                // Adiciona cada child existente em uma lista de Conversas.
+                listaConversas.add(conversa);
+
+                conversasAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+//   *********************  Função que recupera os valores do Firebase e aplica no recycler  ***************************
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarConversas();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        conversasREF.removeEventListener(childEventListenerConversas);
     }
 }
