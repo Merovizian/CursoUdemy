fbff6f8fe6f4daef1b43bc7bccd1364f7d8e8894
Merovizian <ericgmicaela@gmail.com>
2023-10-30 21:42:28 -0300

    V0.0 - Iniciando os Trabalhos

---
+++ Whatsapp/app/src/main/java/hobby/eric/whatsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package hobby.eric.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
